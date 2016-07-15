3248aef762214f5e103dd4efe2c6d2db87b202c9
Tonny Boesenbaek <tonnyb@gmail.com>
2016-07-14 23:51:43 -0400

    Finished homework

--- CEN4370_Week9/MainPage.xaml.cs
+++ CEN4370_Week9/MainPage.xaml.cs
@@ -44,5 +44,46 @@ namespace CEN4370_Week9
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
         }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            double height;
+            double weight;
+            double bmi;
+
+            try
+            {
+                // Convert numbers to doubles
+                height = Convert.ToDouble(heightTextBox.Text);
+                weight = Convert.ToDouble(weightTextBox.Text);
+
+                // determine which formula to use to calculate bmi
+                if ((bool)englishRadioButton.IsChecked)
+                {
+                    bmi = (weight * 703) / (height * height);
+                }
+                else if ((bool)metricRadioButton.IsChecked)
+                {
+                    bmi = weight / (height * height);
+                }
+                else
+                {
+                    throw new Exception();
+                }
+
+                // format to two decimal places
+                //String.Format("{0:0.00}", 123.4567);      // "123.46"
+
+                bmiTextBlock.Text = "Your BMI is: " + String.Format("{0:0.00}", bmi);
+            }
+            catch (FormatException)
+            {
+                bmiTextBlock.Text = "You can only enter numbers for height/weight.";
+            }
+            catch (Exception)
+            {
+                bmiTextBlock.Text = "You have to select English or Metric units.";
+            }
+        }
     }
 }
